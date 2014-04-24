11a6b59d0860d4a3b52f6078b2c0bbbbe532a96d
Ryan <ryan.e.levi@gmail.com>
2014-04-24 12:19:51 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhosPlaying::Application.config.secret_key_base = 'b29a65ca31d87e9e5d1942b0717789499bbca23bd5a0f48753de9824494c2f50db0025cfbd0b571299af36eee5eb3323e4b6bb80957f10a3a0248edea0d1be3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhosPlaying::Application.config.session_store :cookie_store, key: '_whos_playing_session'
